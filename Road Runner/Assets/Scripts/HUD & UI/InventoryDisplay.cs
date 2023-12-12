dc8dce4dfc2071f4367af31877b6e1baa9b9a32e
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2023-12-12 15:41:52 -0800

    Experiments with using grids for inventory
    Seems less useful than I though it would be. Once I'm really expanding the inventory system I may need to use it more...

--- Road Runner/Assets/Scripts/HUD & UI/InventoryDisplay.cs
+++ Road Runner/Assets/Scripts/HUD & UI/InventoryDisplay.cs
@@ -11,28 +11,24 @@ public class InventoryDisplay : MonoBehaviour
     public static InventoryDisplay Instance;
 
     private BaseInventory baseInventory;
-    
-    [SerializeField] private Button button;
-    [SerializeField] private int slotSpacing = 45;
-    
-    [SerializeField] private Button usingSlotButton;
-    [SerializeField] private Image usingSlotImage;
-    
+
+    [SerializeField] private Button button; // Button for inventory slot
+    [SerializeField] private int slotSpacing = 45; // Spacing between inventory slots
+
+    [SerializeField] private Button usingSlotButton; // Button for the slot currently in use
+    [SerializeField] private Image usingSlotImage; // Image for the slot currently in use
+
     private Dictionary<Inventory.InventoryItem, ItemSO> itemSoDictionary;
 
-    [SerializeField] private Button dropButton;
-    
-    [SerializeField] private Image handDisplay;
+    [SerializeField] private Button dropButton; // Button to drop an item
 
-    [SerializeField] private RectTransform inventoryBackdrop;
-    // private RectTransform handDisplayTransform;
-    private Dictionary<int, RectTransform> invetoryDisplays;
-    private Dictionary<int, Image[,]> inventoryImages;
-    //private Button[,] inventoryButtons;
+    [SerializeField] private Image handDisplay; // Display for the item in hand
 
-    private float nextInventoryYPosition = -10;
-    
-    private Vector2 dimensions;
+    [SerializeField] private RectTransform inventoryBackdrop; // Backdrop for the inventory display
+    private Dictionary<int, RectTransform> invetoryDisplays; // Dictionary to hold all inventory displays
+    private Dictionary<int, Image[,]> inventoryImages; // Dictionary to hold all inventory images
+
+    private float nextInventoryYPosition = -10; // Position for the next inventory display
 
     private void Awake()
     {
@@ -56,12 +52,12 @@ public class InventoryDisplay : MonoBehaviour
     {
         baseInventory = baseInventoryRef;
         itemSoDictionary = ItemSpawner.ItemDictionary;
-        
+
         handDisplay.sprite = itemSoDictionary[Inventory.InventoryItem.Empty].GetSprite();
-        
+
         usingSlotButton.onClick.AddListener(() => HandleUsingSlotClick());
         dropButton.onClick.AddListener(() => DropItem());
-        CreateDisplayArrays(0, width, height);
+        CreateDisplayArrays(0, width, height); // Create the display for the base inventory
     }
 
     public void CreateInventoryDisplay(int inventoryKey, int width, int height)
@@ -72,7 +68,7 @@ public class InventoryDisplay : MonoBehaviour
             return;
         }
 
-        CreateDisplayArrays(inventoryKey, width, height);
+        CreateDisplayArrays(inventoryKey, width, height); // Create the display for the additional inventory
     }
 
     private void CreateDisplayArrays(int inventoryKey, int width, int height)
@@ -81,28 +77,28 @@ public class InventoryDisplay : MonoBehaviour
         inventoryImages.Add(inventoryKey, new Image[width, height]);
 
         invetoryDisplays.Add(inventoryKey, Instantiate(inventoryBackdrop, transform.position, transform.rotation, transform));
-        invetoryDisplays[inventoryKey].anchoredPosition = new Vector2(10, nextInventoryYPosition);
+        invetoryDisplays[inventoryKey].anchoredPosition = new Vector2(10, nextInventoryYPosition); // Positioning the inventory display
 
         int inventoryHeight = height * slotSpacing + 5;
-        invetoryDisplays[inventoryKey].sizeDelta = new Vector2(width * slotSpacing + 5, inventoryHeight);
+        invetoryDisplays[inventoryKey].sizeDelta = new Vector2(width * slotSpacing + 5, inventoryHeight); // Setting the size of the inventory display
 
-        nextInventoryYPosition -= inventoryHeight + 10; // Adust position of next iventory!
+        nextInventoryYPosition -= inventoryHeight + 10; // Adjusting the position for the next inventory display
 
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
                 // LAGGGG ZONNEEEE... maybe....
-                inventoryButtons[x,y] = Instantiate(button, transform.position, transform.rotation, invetoryDisplays[inventoryKey]);
-                inventoryButtons[x,y].GetComponent<RectTransform>().anchoredPosition = new Vector2(x * slotSpacing + 5, y * -slotSpacing - 5);
-                
-                inventoryImages[inventoryKey][x,y] = inventoryButtons[x, y].transform.Find("Image").GetComponent<Image>();
-                inventoryImages[inventoryKey][x,y].sprite = itemSoDictionary[Inventory.InventoryItem.Empty].GetSprite();
-                
+                inventoryButtons[x, y] = Instantiate(button, transform.position, transform.rotation, invetoryDisplays[inventoryKey]);
+                // inventoryButtons[x, y].GetComponent<RectTransform>().anchoredPosition = new Vector2(x * slotSpacing + 5, y * -slotSpacing - 5); // Positioning the inventory slot
+
+                inventoryImages[inventoryKey][x, y] = inventoryButtons[x, y].transform.Find("Image").GetComponent<Image>();
+                inventoryImages[inventoryKey][x, y].sprite = itemSoDictionary[Inventory.InventoryItem.Empty].GetSprite(); // Setting the default image for the inventory slot
+
                 int xValue = x;
                 int yValue = y;
                 int inventoryIndexValue = inventoryKey;
-                inventoryButtons[x,y].onClick.AddListener(() => {HandleInventoryClick(inventoryIndexValue, xValue, yValue);});
+                inventoryButtons[x, y].onClick.AddListener(() => { HandleInventoryClick(inventoryIndexValue, xValue, yValue); });
             }
         }
     }
@@ -111,7 +107,7 @@ public class InventoryDisplay : MonoBehaviour
     {
         float removedInventoryHeight = invetoryDisplays[inventoryKey].sizeDelta.y + 10;
 
-        nextInventoryYPosition += removedInventoryHeight;
+        nextInventoryYPosition += removedInventoryHeight; // Adjusting the position for the next inventory display after removing an inventory display
 
         Destroy(invetoryDisplays[inventoryKey].gameObject);
         inventoryImages.Remove(inventoryKey);
@@ -121,33 +117,33 @@ public class InventoryDisplay : MonoBehaviour
     private void HandleInventoryClick(int inventoryIndex, int x, int y)
     {
         Debug.Log("Clicked inventory square X: " + x + " Y: " + y);
-        
+
         Inventory.InventoryItem itemForSlot = baseInventory.ClickOnSlot(inventoryIndex, x, y, out Inventory.InventoryItem handItem);
 
-        inventoryImages[inventoryIndex][x,y].sprite = itemSoDictionary[itemForSlot].GetSprite();
-        handDisplay.sprite = itemSoDictionary[handItem].GetSprite();
+        inventoryImages[inventoryIndex][x, y].sprite = itemSoDictionary[itemForSlot].GetSprite(); // Updating the image for the clicked inventory slot
+        handDisplay.sprite = itemSoDictionary[handItem].GetSprite(); // Updating the image for the item in hand
     }
 
     public void UpdateItemSlot(int inventoryKey, int x, int y, Inventory.InventoryItem inventoryItem)
     {
-        inventoryImages[inventoryKey][x,y].sprite = itemSoDictionary[inventoryItem].GetSprite();
+        inventoryImages[inventoryKey][x, y].sprite = itemSoDictionary[inventoryItem].GetSprite(); // Updating the image for the inventory slot
     }
 
     private void HandleUsingSlotClick()
     {
         Inventory.InventoryItem usingItem = baseInventory.SetUsing(out Inventory.InventoryItem handItem);
-        usingSlotImage.sprite = itemSoDictionary[usingItem].GetSprite();
-        handDisplay.sprite = itemSoDictionary[handItem].GetSprite();
+        usingSlotImage.sprite = itemSoDictionary[usingItem].GetSprite(); // Updating the image for the slot currently in use
+        handDisplay.sprite = itemSoDictionary[handItem].GetSprite(); // Updating the image for the item in hand
     }
 
     public void ResetUsingSlot()
     {
-        usingSlotImage.sprite = itemSoDictionary[Inventory.InventoryItem.Empty].GetSprite();
+        usingSlotImage.sprite = itemSoDictionary[Inventory.InventoryItem.Empty].GetSprite(); // Resetting the image for the slot currently in use
     }
 
     private void DropItem()
     {
-        handDisplay.sprite = itemSoDictionary[Inventory.InventoryItem.Empty].GetSprite();
+        handDisplay.sprite = itemSoDictionary[Inventory.InventoryItem.Empty].GetSprite(); // Resetting the image for the item in hand after dropping an item
         baseInventory.DropItem();
     }
 }
