21be10cd5daffdf9c83c9fd6b6024a71fac61f83
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2023-12-17 00:05:22 -0800

    New Object Spawning system working with weapons
    - New object spawning system working with guns
    - Uses sprinkles to make sure each object spawning zone in the sprinkle is full

---
+++ Road Runner/Assets/Scripts/Sprinkles & Spawning/SpawnZones/SpawnZone.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZone : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] protected float maxSpawnedObjects;
+    [SerializeField] protected SpawnedObject[] objectsToSpawn;
+
+    protected int numSpawnedObjects;
+
+    protected BoxCollider boxCollider;
+    protected Bounds spawnBounds;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider>();
+    }
+
+    public bool IsFull()
+    {
+        return numSpawnedObjects >= maxSpawnedObjects;
+    }
+
+    public void RemoveSpawnedObject()
+    {
+        numSpawnedObjects--;
+    }
+    
+    public virtual void SpawnRandomObject()
+    {
+        if (IsFull())
+        {
+            Debug.LogWarning("Tried to spawn in full spawn zone");
+            return;
+        }
+
+        numSpawnedObjects++;
+
+        int randomIndex = Random.Range(0, objectsToSpawn.Length);
+        Vector3 randomPoint = GetRandomPointInBounds();
+
+        ObjectSpawner.Instance.SpawnObject(objectsToSpawn[randomIndex], randomPoint, this);
+    }
+
+    protected Vector3 GetRandomPointInBounds()
+    {
+        spawnBounds = boxCollider.bounds;
+
+        float x = Random.Range(spawnBounds.min.x, spawnBounds.max.x);
+        float y = Random.Range(spawnBounds.min.y, spawnBounds.max.y);
+        float z = Random.Range(spawnBounds.min.z, spawnBounds.max.z);
+
+        return new Vector3(x, y, z);
+    }
+}
--- Road Runner/Assets/Scripts/Terrain Generation/Sprinkle.cs
+++ Road Runner/Assets/Scripts/Terrain Generation/Sprinkle.cs
@@ -5,8 +5,83 @@ using UnityEngine;
 
 public class Sprinkle : MonoBehaviour
 {
-    [SerializeField] int flatRadius;
-    [SerializeField] int blendRadius;
+    [Header("Terrain Generation")]
+    [SerializeField] int flatRadius = 16;
+    [SerializeField] int blendRadius = 32;
+
+    [Header("Item Spawning")]
+    [SerializeField] private bool spawnItems = true;
+    [SerializeField] private float itemSpawnCheckInterval = 90f;
+    [SerializeField] private SpawnZone[] itemSpawnZones;
+
+    [Header("Enemy NPC Spawning")]
+    [SerializeField] private bool spawnEnemies = true;
+    [SerializeField] private float enemyNPCSpawnCheckInterval = 90f;
+    [SerializeField] private SpawnZone[] enemyNPCSpawnZones;
+
+    [Header("Vehicle Spawning")]
+    [SerializeField] private bool spawnVehicles = true;
+    [SerializeField] private float vehicleSpawnCheckInterval = 180f;
+    [SerializeField] private SpawnZone[] vehicleSpawnZones;
+
+    private int numNaturalItems;
+    private int numEnemyNPCs;
+
+    private void Start()
+    {
+        TerrainManager.onTerrainGenerated.AddListener(() =>
+        {
+            if (!TerrainManager.Instance.IsServer)
+            { 
+                Debug.LogWarning("Disableing Sprinkle on Client");
+                foreach (var itemSpawnZone in itemSpawnZones)
+                    itemSpawnZone.enabled = false;
+                foreach (var enemyNPCSpawnZone in enemyNPCSpawnZones)
+                    enemyNPCSpawnZone.enabled = false;
+                foreach (var vehicleSpawnZone in vehicleSpawnZones)
+                    vehicleSpawnZone.enabled = false;
+
+                enabled = false;
+
+                return;
+            }
+
+            if (spawnItems)
+                StartCoroutine(SpawnRoutine(itemSpawnCheckInterval, itemSpawnZones));
+            if (spawnEnemies)                
+                StartCoroutine(SpawnRoutine(enemyNPCSpawnCheckInterval, enemyNPCSpawnZones));
+            if (spawnVehicles)
+                StartCoroutine(SpawnRoutine(vehicleSpawnCheckInterval, vehicleSpawnZones));
+        });
+    }
+
+    private IEnumerator SpawnRoutine(float spawnInterval, SpawnZone[] spawnZones)
+    {
+        while (true)
+        { 
+            yield return new WaitForSeconds(spawnInterval);
+
+            TrySpawnItem(spawnZones);
+        }
+    }
+
+    private void TrySpawnItem(SpawnZone[] spawnZones)
+    {
+        List<int> avialableIndexes = new List<int>();
+        for (int i = 0; i < spawnZones.Length; i++)
+        {
+            if (!spawnZones[i].IsFull())
+                avialableIndexes.Add(i);
+        }
+
+        if (avialableIndexes.Count <= 0)
+            return;
+
+        int randomIndex = avialableIndexes[UnityEngine.Random.Range(0, avialableIndexes.Count)];
+        spawnZones[randomIndex].SpawnRandomObject();
+    }
+
+    #region Terrain Generation
 
     public int FlatRadius
     {
@@ -19,4 +94,6 @@ public class Sprinkle : MonoBehaviour
         get { return blendRadius; }
         private set { }
     }
+
+    #endregion
 }
