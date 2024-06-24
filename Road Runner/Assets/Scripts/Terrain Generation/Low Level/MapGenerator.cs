1d0f3add504790eee52977d78388113ab6f97525
Martin Allsbrook <78395106+MartinAllsbrook@users.noreply.github.com>
2024-06-23 19:04:44 -0700

    Pushes just to be safe
     because I'm about to edit important stuff i haven't touched in a while

--- Road Runner/Assets/Scripts/Terrain Generation/Low Level/MapGenerator.cs
+++ Road Runner/Assets/Scripts/Terrain Generation/Low Level/MapGenerator.cs
@@ -8,30 +8,33 @@ using Debug = UnityEngine.Debug;
 
 public class MapGenerator : MonoBehaviour
 {
+    [Header("Global")]
     [SerializeField] int globalScaleDown = 512;
-
+    
     [Header("Height")]
-    [SerializeField] private float[] heightOctaves;
-    [SerializeField] private float heightRedistributionFactor;
-    [SerializeField] private int maxHeight;
+    [SerializeField] float[] heightOctaves;
+    [SerializeField] float heightRedistributionFactor;
+    [SerializeField] int maxHeight;
     
     [Header("Moisture")]
-    [SerializeField] private float[] moistureOctaves;
-    [SerializeField] private float moistureRedistributionFactor;
-    [SerializeField] private int maxMoisture;
+    [SerializeField] float[] moistureOctaves;
+    [SerializeField] float moistureRedistributionFactor;
+    [SerializeField] int maxMoisture;
 
     [Header("Strangeness")]
-    [SerializeField] private float[] strangenessOctaves;
-    [SerializeField] private float strangenessRedistributionFactor;
-    [SerializeField] private int maxStrangeness;
+    [SerializeField] float[] strangenessOctaves;
+    [SerializeField] float strangenessRedistributionFactor;
+    [SerializeField] int maxStrangeness;
     
     [Header("Density")]
-    [SerializeField] private float[] densityOctaves;
-    [SerializeField] private float densityRedistributionFactor;
-    [SerializeField] private int maxDensity;
+    [SerializeField] float[] densityOctaves;
+    [SerializeField] float densityRedistributionFactor;
+    [SerializeField] int maxDensity;
 
     public delegate void GenericDelegate();
     public delegate void GenericDelegate<T>(T variable);
+
+    int milisecondsTillYield = 10;
 
     #region Map Generation
 
@@ -49,13 +52,13 @@ public class MapGenerator : MonoBehaviour
         float[,] strangenessMap = new float[size, size];
         float[,] densityMap = new float[size, size];
 
-        yield return GenerateNoiseCoroutine(seeds[0], Vector2Int.zero, heightOctaves, heightRedistributionFactor, maxHeight, size, data => { heightMap = data; });
+        yield return GenerateGenraricMapRoutine(seeds[0], heightOctaves, heightRedistributionFactor, maxHeight, size, data => { heightMap = data; });
 
-        yield return GenerateNoiseCoroutine(seeds[1], Vector2Int.zero, moistureOctaves, moistureRedistributionFactor, maxMoisture, size, data => { moistureMap = data; });
+        yield return GenerateGenraricMapRoutine(seeds[1], moistureOctaves, moistureRedistributionFactor, maxMoisture, size, data => { moistureMap = data; });
 
-        yield return GenerateNoiseCoroutine(seeds[2], Vector2Int.zero, strangenessOctaves, strangenessRedistributionFactor, maxStrangeness, size, data => { strangenessMap = data; });
+        yield return GenerateGenraricMapRoutine(seeds[2], strangenessOctaves, strangenessRedistributionFactor, maxStrangeness, size, data => { strangenessMap = data; });
 
-        yield return GenerateNoiseCoroutine(seeds[3], Vector2Int.zero, densityOctaves, densityRedistributionFactor, maxDensity, size, data => { densityMap = data; });
+        yield return GenerateGenraricMapRoutine(seeds[3], densityOctaves, densityRedistributionFactor, maxDensity, size, data => { densityMap = data; });
 
         terrainData.SetMaps(heightMap, moistureMap, strangenessMap, densityMap);
 
@@ -64,81 +67,18 @@ public class MapGenerator : MonoBehaviour
         yield return null;
     }
 
-
-/*    // Step 1: Generate Heights
-    public void GenerateMap(Vector2Int tile, int[] seeds, TerrainData chunkData, GenericDelegate finalCallback)
-    {
-        _size = chunkData.Size;
-
-        GenerateNoise(seeds[0], tile, heightOctaves, heightRedistributionFactor, maxHeight, () =>
-        {
-            GenerateMap(tile, seeds, _generatedMap, chunkData, finalCallback);
-        });
-    }
-
-    // Step 2: Generate Moisture
-    private void GenerateMap(Vector2Int tile, int[] seeds, float[,] heightMap, TerrainData chunkData, GenericDelegate finalCallback)
-    {
-        GenerateNoise(seeds[1], tile, moistureOctaves, moistureRedistributionFactor, maxMoisture, () =>
-        {
-            GenerateMap(tile, seeds, heightMap, _generatedMap, chunkData, finalCallback);
-        });
-    }
-
-    // Step 3: Generate Strangeness
-    private void GenerateMap(Vector2Int tile, int[] seeds, float[,] heightMap, float[,] moistureMap, TerrainData chunkData, GenericDelegate finalCallback)
-    {
-        GenerateNoise(seeds[2], tile, strangenessOctaves, strangenessRedistributionFactor, maxStrangeness, () =>
-        {
-            GenerateMap(tile, seeds, heightMap, moistureMap, _generatedMap, chunkData, finalCallback);
-        });
-    }
-    
-    // Step 4: Generate Density
-    private void GenerateMap(Vector2Int tile, int[] seeds, float[,] heightMap, float[,] moistureMap, float[,] strangenessMap, TerrainData chunkData, GenericDelegate finalCallback)
-    {
-        GenerateNoise(seeds[3], tile, densityOctaves, densityRedistributionFactor, maxDensity, () =>
-        {
-            GenerateMap(heightMap, moistureMap, strangenessMap, _generatedMap, chunkData, finalCallback);
-        });
-    }
-    
-    // Step 5: Compile into biome map
-    private void GenerateMap(float[,] heightMap, float[,] moistureMap, float[,] strangenessMap, float[,] densityMap, TerrainData chunkData, GenericDelegate finalCallback)
-    {
-        chunkData.SetMaps(heightMap, moistureMap, strangenessMap, densityMap);
-        
-        finalCallback();
-    }
-
-    #endregion
-    
-    #region Noise Generation
-
-    private void GenerateNoise(int seed, Vector2Int position, float[] octaves, float redistributionFactor, int maxValue, GenericDelegate onFinishedCallback)
-    {
-        StartCoroutine(GenerateNoiseCoroutine(seed, position, octaves, redistributionFactor, maxValue, data =>
-            {
-                _generatedMap = data;
-                onFinishedCallback?.Invoke();
-            }
-        ));
-    }*/
-
-    IEnumerator GenerateNoiseCoroutine(int seed, Vector2Int position, float[] octaves, float redistributionFactor, int maxValue, int size, GenericDelegate<float[,]> callback)
+    IEnumerator GenerateGenraricMapRoutine(int seed, float[] octaves, float redistributionFactor, int maxValue, int size, GenericDelegate<float[,]> callback)
     {
         Stopwatch timer = new Stopwatch();
         timer.Start();
 
-        float[,] noise = new float[size, size];
+        float[,] map = new float[size, size];
 
-        Vector2 seedOffset = new Vector2(seed, seed);
-        Vector2 positionOffset = position * (size - 1);
-        Vector2 offset = positionOffset + seedOffset;
+        Vector2 offset = new Vector2(seed, seed);
 
         for (int z = 0; z < size; z++)
         {
-            if (timer.ElapsedMilliseconds > 3)
+            if (timer.ElapsedMilliseconds > milisecondsTillYield)
             {
                 yield return null;
                 timer.Reset();
@@ -147,13 +87,13 @@ public class MapGenerator : MonoBehaviour
 
             for (int x = 0; x < size; x++)
             {
-                noise[x, z] = (maxValue * CompileNoise(x, z, offset, octaves, redistributionFactor));
+                map[x, z] = CompileNoise(x, z, offset, octaves, redistributionFactor) * maxValue;
             }
         }
-        timer.Stop();
-        // Debug.Log("Total time: " + timer.ElapsedMilliseconds);
         
-        callback(noise);
+        timer.Stop();
+
+        callback(map);
         yield return null;
     }
     
